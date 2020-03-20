9fd5f85d327a3a72cff699aa8799e7fb32298dff
javeethbasha <javeethbashacse@gmail.com>
2020-03-20 13:57:46 +0530

    Created Model to store the Event Data and Updated the Setup code

---
+++ BMSWeekendSports/src/Models/SportsDetail.java
@@ -0,0 +1,53 @@
+package Models;
+
+public class SportsDetail {
+    private String _name;
+    private String _month;
+    private String _day;
+    private String _category;
+    private String _amount;
+
+
+    public SportsDetail() {
+    }
+
+    public String get_name() {
+        return _name;
+    }
+
+    public void set_name(String _name) {
+        this._name = _name;
+    }
+
+    public String get_month() {
+        return _month;
+    }
+
+    public void set_month(String _month) {
+        this._month = _month;
+    }
+
+    public String get_day() {
+        return _day;
+    }
+
+    public void set_day(String _day) {
+        this._day = _day;
+    }
+
+    public String get_category() {
+        return _category;
+    }
+
+    public void set_category(String _category) {
+        this._category = _category;
+    }
+
+    public String get_amount() {
+        return _amount;
+    }
+
+    public void set_amount(String _amount) {
+        this._amount = _amount;
+    }
+}
--- BMSWeekendSports/src/Repository/BaseClass.java
+++ BMSWeekendSports/src/Repository/BaseClass.java
@@ -1,34 +1,59 @@
 package Repository;
 
+import Models.SportsDetail;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class BaseClass {
+
+    static WebDriver driver = new ChromeDriver();
+
     public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        login(driver);
-        getPrice(driver);
+        setDriverProperty();
+        login();
+        getPrice();
+        closeDriver();
     }
 
-    public static void login(WebDriver driver) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver");
-        driver = new ChromeDriver();
+    public static void setDriverProperty(){
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+    }
+
+    public static void login() throws InterruptedException {
         driver.get("https://in.bookmyshow.com/bengaluru/sports?dayGroup=Weekend&priceGroup=min0max500");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollBy(0,1000)");
     }
 
-    public static void getPrice(WebDriver driver) throws InterruptedException {
-        Thread.sleep(10000);
-        WebElement price = driver.findElement(By.xpath("//div[@class= 'bd fd fe']"));
+    public static void getPrice(){
+        HashSet<SportsDetail> eventDetails = null;
+        List<WebElement> totalEvents = driver.findElements(By.xpath("//div[@class = 'ax c es et eu ev k v']"));
+        List<WebElement> price = driver.findElements(By.xpath("//div[contains(@class, 'bd fd fe')]"));
+        List<WebElement> day = driver.findElements(By.xpath("//div[@class = 'bd ey']"));
+        List<WebElement> month = driver.findElements(By.xpath("//div[@class = 'bd ch ez']"));
+        List<WebElement> category = driver.findElements(By.xpath("//div[@class = 'ap bd fe']"));
+        List<WebElement> name = driver.findElements(By.xpath("//div[@class = 'al b ch fb fc']"));
+
+        for(int i = 0; i < totalEvents.size(); i++){
+            SportsDetail sportsDetail = new SportsDetail();
+            sportsDetail.set_day(day.get(i).getText());
+            sportsDetail.set_amount(price.get(i).getText());
+            sportsDetail.set_month(month.get(i).getText());
+            sportsDetail.set_category(category.get(i).getText());
+            sportsDetail.set_name(name.get(i).getText());
+
+            eventDetails.add(sportsDetail);
+        }
     }
 
+    public static void closeDriver(){
+        driver.close();
+    }
 
 }
